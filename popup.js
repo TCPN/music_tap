05e98aadc40a1a4a45a7bade36b712812d8ffbca
TCPN <boyenen@gmail.com>
2018-04-18 01:18:45 +0800

    simpler popup close mechanism

--- popup.js
+++ popup.js
@@ -1,10 +1,21 @@
-
+openingPopups = [];
 function popupHandler(evt){
 	let clicked = findValidParent(evt.target, 'popupTarget');
 	if(!clicked)
 		return;
 	let target = document.querySelector(clicked.dataset.popupTarget);
 	target.classList.add('show');
+	openingPopups.push(target);
 }
-
+function popupBlurHandler(evt){
+	openingPopups = openingPopups.filter((dom)=>{
+		if(!dom.contains(evt.target)){
+			dom.classList.remove('show');
+			return false;
+		}
+		return true;
+	});
+}
 window.addEventListener('click', popupHandler);
+window.addEventListener('touchstart', popupBlurHandler);
+window.addEventListener('mousedown', popupBlurHandler);
