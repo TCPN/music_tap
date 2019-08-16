2e2117b8ff11bebe3be592003128c38e3fe7bcfe
TCPN <boyenen@gmail.com>
2019-08-17 04:32:10 +0800

    [fix] fix the findValidParent by checking the startdom != null

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 function findValidParent(startdom, datasetItemName){
 	var dom = startdom;
-	while(dom.dataset[datasetItemName] == undefined){
+	while(dom && dom.dataset[datasetItemName] == undefined){
 		dom = dom.parentElement;
 		if(dom == document.body)
 			return null;
